f955f33ee9b4e951e0111fab91eec43ffed38063
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-17 02:54:49 +0300

    Updated execute_command.c for Task 3

--- execute_command.c
+++ execute_command.c
@@ -1,39 +1,91 @@
 #include "header_file.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int execute_command(char *command)
 {
-    pid_t pid = fork();
+    char *path = getenv("PATH");
+    char *token, *cmd;
+    char path_copy[MAX_INPUT_SIZE];
+    int command_found = 0; /* Flag to check if the command exists */
 
-    if (pid < 0)
+    if (path == NULL)
     {
-        perror("Fork failed");
+        perror("Error getting PATH");
         return -1;
     }
 
-    if (pid == 0)
+    strncpy(path_copy, path, sizeof(path_copy));
+    path_copy[sizeof(path_copy) - 1] = '\0';
+
+    token = strtok(path_copy, ":");
+
+    while (token != NULL)
     {
-        char *command_name = strtok(command, " "); /* Extract the command name */
-        char *argv[MAX_ARG_SIZE];                  /* An array to store command and arguments */
-
-        int i = 0;
-
-        while (command_name != NULL)
+        cmd = malloc(strlen(token) + strlen(command) + 2);
+        if (cmd == NULL)
         {
-            argv[i] = command_name;
-            i++;
-            command_name = strtok(NULL, " ");
+            perror("Malloc failed");
+            return -1;
+        }
+        sprintf(cmd, "%s/%s", token, command);
+
+        if (access(cmd, X_OK) == 0)
+        {
+            /* Command is accessible, execute it */
+            free(cmd);
+            command_found = 1; /* Set the flag to indicate command existence */
+            break;
         }
 
-        argv[i] = NULL; /* Null-terminate the array */
+        free(cmd);
+        token = strtok(NULL, ":");
+    }
 
-        if (execvp(argv[0], argv) == -1)
-            return -1; /* Command not found */
+    if (command_found)
+    {
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            perror("Fork failed");
+            return -1;
+        }
+
+        if (pid == 0)
+        {
+            char *command_name = strtok(command, " "); /* Extract the command name */
+            char *argv[MAX_ARG_SIZE];                  /* An array to store command and arguments */
+            int i = 0;
+
+            while (command_name != NULL)
+            {
+                argv[i] = command_name;
+                i++;
+                command_name = strtok(NULL, " ");
+            }
+
+            argv[i] = NULL; /* Null-terminate the array */
+
+            if (execvp(argv[0], argv) == -1)
+            {
+                perror("execvp failed");
+                return -1; /* Command not found */
+            }
+        }
+        else
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        return 0;
     }
     else
     {
-        int status;
-        waitpid(pid, &status, 0);
+        fprintf(stderr, "Command not found: %s\n", command);
+        return -1;
     }
-
-    return 0;
 }
