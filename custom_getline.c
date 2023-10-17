ddde9c6abcb5ff340edbaf7b2d10c97034e40312
Mutai Kelvin <90502040+Mutai05@users.noreply.github.com>
2023-10-17 03:52:10 +0300

    Added new file custom_getline.c

---
+++ custom_getline.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define MAX_INPUT_SIZE 1024
+
+ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
+{
+    char *buffer = NULL;
+    size_t size = 0;
+    size_t len = 0;
+    int c;
+
+    if (lineptr == NULL || n == NULL)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    while (1)
+    {
+        c = fgetc(stream);
+
+        if (c == EOF || c == '\n')
+        {
+            if (c == EOF && (!buffer || len == 0))
+            {
+                return -1; /* No more data to read */
+            }
+            if (len + 1 >= size)
+            {
+                size = (len + 1) * 2;
+                char *new_buffer = (char *)realloc(buffer, size);
+                if (!new_buffer)
+                {
+                    free(buffer);
+                    return -1; /* Allocation error */
+                }
+                buffer = new_buffer;
+            }
+            if (c == EOF)
+            {
+                buffer[len] = '\0';
+                break;
+            }
+            buffer[len] = '\n';
+            buffer[len + 1] = '\0';
+            break;
+        }
+
+        if (len + 1 >= size)
+        {
+            size = (len + 1) * 2;
+            char *new_buffer = (char *)realloc(buffer, size);
+            if (!new_buffer)
+            {
+                free(buffer);
+                return -1; /* Allocation error */
+            }
+            buffer = new_buffer;
+        }
+        buffer[len] = (char)c;
+        buffer[len + 1] = '\0';
+        len++;
+    }
+
+    *lineptr = buffer;
+    *n = size;
+
+    return len;
+}
